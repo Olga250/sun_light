3550b2f1c9f6616c0040284a0aa91f24886f8af3
Yuor Name <you@example.com>
2016-11-22 05:11:00 +0000

    views

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse  
 # Create your views here.  
-def test(request, *args, **kwargs):  
-    return HttpResponse('OK')  
+#def test(request, *args, **kwargs):  
+ #   return HttpResponse('OK')  
+def main(request):
+    return HttpResponse('OK')
+
 
 
