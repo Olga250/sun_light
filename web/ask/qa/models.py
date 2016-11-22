08c0f6d41cffc85f1d94a120a7d8365bc5782d0b
Your Name <you@example.com>
2016-11-22 04:26:12 +0000

    models

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+class QuestionManager(models.Manager):                                          
+        def new():                                                              
+                pass                                                            
+        def popular():                                                          
+              pass 
+class Question(models.Model):  
+class Question(models.Model):                                                   
+   objects = QuestionManager() 
+    title = models.CharField(max_length=255)  
+    text = models.TextField()  
+    rating = models.IntegerField()  
+    added_at = models.DateTimeField(auto_now=True)  
+    author = models.ForeignKey(User, related_name="question_author")  
+    likes = models.ManyToManyField(User)  
+class Answer(models.Model):  
+    text = models.TextField()  
+    added_at = models.DateTimeField(auto_now=True)  
+    question = models.ForeignKey(Question)  
+    author = models.ForeignKey(User, related_name="answer_author")  
+
+
+
