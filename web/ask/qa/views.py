9fc67a4eeda4f69e6d5ed5e60386854d67c6d181
Yuor Name <you@example.com>
2016-11-22 05:28:39 +0000

    views

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -5,6 +5,18 @@ from django.http import HttpResponse
  #   return HttpResponse('OK')  
 def main(request):
     return HttpResponse('OK')
+def draw_question(request, q_id):
+    try:
+        question = Question.objects.get(id = q_id)
+    except Question.DoesNotExist:
+        raise Http404
+    return render(request, 'ask/question.html', {
+       'question' : question,
+       'title' : question.title,
+       'text' : question.text, 
+       })
+
+
 
 
 
